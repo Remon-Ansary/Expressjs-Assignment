ac05373d2f0829b4a2343336b46ea04bad848e34
Remon-Ansary <rasel15-10514@diu.edu.bd>
2021-12-29 17:36:59 +0600

    prblm fixed start

--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var axios = require("axios").default;
-/* GET home page. */
-// router.get("/", function (req, res, next) {
-//   var country = "Bangladesh";
-//   var city = "Dhaka";
-//   axios
-//     .get(
-//       "http://api.weatherapi.com/v1/current.json?key=24fd1c53215c4f9dbc195356212812&q=" +
-//         country +
-//         "&q=" +
-//         city +
-//         "&aqi=no"
-//     )
-//     .then((response) => {
-//       console.log(response.data);
-//       console.log(response.data);
-//       var jsonObject = JSON.stringify(response.data);
-//       console.log(jsonObject);
-//       var object = JSON.parse(jsonObject);
-
-//       console.log(object.keys);
-
-//       // for (x in object) {
-//       //   res.render("index", { title: object[x].name, test: object[x].country });
-//       //   // res.render("index", { title: object[x].country });
-//       // }
-//       // for (x in object) {
-//       res.render("index", { title: object.current.cloud });
-//       //}
-
-//       // res.render("index", { title: "" + Object.keys(object) });
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// });
-
-router.get("/all/:country/:city", function (req, res, next) {
-  var country = req.params.country;
-  var city = req.params.city;
-  axios
-    .get(
-      "http://api.weatherapi.com/v1/current.json?key=24fd1c53215c4f9dbc195356212812&q=" +
-        country +
-        "&q=" +
-        city +
-        "&aqi=no"
-    )
-    .then((response) => {
-      console.log(response.data);
-      console.log(response.data);
-      var jsonObject = JSON.stringify(response.data);
-      console.log(jsonObject);
-      var object = JSON.parse(jsonObject);
-
-      // console.log(object.keys);
-
-      // for (x in object) {
-      //   res.render("index", { title: object[x].name, test: object[x].country });
-      //   // res.render("index", { title: object[x].country });
-      // }
-
-      res.render("index", {
-        location: object.location.name,
-        country: object.location.country,
-        time: object.location.localtime,
-        temperature: object.current.temp_c,
-        text: object.current.condition.text,
-        wind: object.current.wind_mph,
-        humidity: object.current.humidity,
-        icon: object.current.condition.icon,
-      });
-      //  res.render("index", { name: object.current.feelslike_f });
-      console.log(object.current.cloud);
-      console.log(object.current.feelslike_f);
-
-      // res.render("index", { title: "" + Object.keys(object) });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+var axios = require("axios");
+var app = express();
 
 router.get("/all/:country/:city/:date", function (req, res, next) {
   var country = req.params.country;
@@ -89,7 +9,7 @@ router.get("/all/:country/:city/:date", function (req, res, next) {
   var date = req.params.date;
   axios
     .get(
-      "http://api.weatherapi.com/v1/history.json?key=24fd1c53215c4f9dbc195356212812&q=" +
+      "http://api.weatherapi.com/v1/history.json?key=2990633566b04f7aac194136212912&q=" +
         country +
         "&q=" +
         city +
@@ -97,40 +17,89 @@ router.get("/all/:country/:city/:date", function (req, res, next) {
         date
     )
     .then((response) => {
-      console.log(response.data);
-      console.log(response.data);
+      // console.log(response.data);
       var jsonObject = JSON.stringify(response.data);
-      console.log(jsonObject);
+      // console.log(jsonObject);
       var object = JSON.parse(jsonObject);
+      // console.log(object);
+      var location = object.location;
+      console.log(location);
+      var forecast = object.forecast.forecastday;
+      console.log(forecast);
+      var sum1 = 0;
+      var sum2 = 0;
+      var sum3 = 0;
+      var sum4 = 0;
+      var sum1_f = 0;
 
-      // console.log(object.keys);
+      var sum2_f = 0;
+      let temp_c_1 =
+        (temp_c_2 =
+        temp_c_3 =
+        temp_c_4 =
+        temp_c_4 =
+        temp_f_1 =
+        temp_f_2 =
+        temp_f_3 =
+        temp_f_4 =
+          []);
+      var sum3_f = 0;
+      var sum4_f = 0;
+      var max1 = forecast[0].hour[0].temp_c;
+      var min1 = forecast[0].hour[0].temp_c;
+      var max2 = forecast[0].hour[0].temp_c;
+      for (var i = 0; i < 24; i++) {
+        console.log(forecast[0].hour[i].temp_c);
+        //sum= sum+forecast[0].hour[i].temp_c;
 
-      // for (x in object) {
-      //   res.render("index", { title: object[x].name, test: object[x].country });
-      //   // res.render("index", { title: object[x].country });
-      // }
+        if (i >= 0 && i < 6) {
+          temp_c_1 = [...temp_c_1, forecast[0].hour[i].temp_c];
+          temp_f_1 = [...temp_f_1, forecast[0].hour[i].temp_f];
+
+          // sum1 = sum1 + forecast[0].hour[i].temp_c;
+          // sum1_f = sum1_f + forecast[0].hour[i].temp_f;
+        } else if (i >= 6 && i < 12) {
+          temp_c_2 = [...temp_c_2, forecast[0].hour[i].temp_c];
+          temp_f_2 = [...temp_f_2, forecast[0].hour[i].temp_f];
+
+          // sum2 = sum2 + forecast[0].hour[i].temp_c;
+          // sum2_f = sum2_f + forecast[0].hour[i].temp_f;
+        } else if (i >= 12 && i < 18) {
+          temp_c_3 = [...temp_c_3, forecast[0].hour[i].temp_c];
+          temp_f_3 = [...temp_f_3, forecast[0].hour[i].temp_f];
+          // sum3 = sum3 + forecast[0].hour[i].temp_c;
+          // sum3_f = sum3_f + forecast[0].hour[i].temp_f;
+        } else if (i >= 18 && i < 24) {
+          temp_c_4 = [...temp_c_4, forecast[0].hour[i].temp_c];
+          temp_f_4 = [...temp_f_4, forecast[0].hour[i].temp_f];
+          // sum4 = sum4 + forecast[0].hour[i].temp_c;
+          // sum4_f = sum4_f + forecast[0].hour[i].temp_f;
+        }
+      }
+      let max_temp_c_1 = Math.max(...temp_c_1);
+
+      let min_temp_c_1 = Math.min(...temp_c_1);
+
+      let max_temp_c_2 = Math.max(...temp_c_2);
+
+      let min_temp_c_2 = Math.min(...temp_c_2);
+
+      // console.log(temp_c_1);
+
+      console.log("max" + max_temp_c_1);
+
+      // console.log(temp_c_1);
+
+      console.log("min" + min_temp_c_1);
 
       res.render("index", {
-        location: object.location.name,
-        country: object.location.country,
-        time: object.location.localtime,
-        temperature: object.current.temp_c,
-        text: object.current.condition.text,
-        wind: object.current.wind_mph,
-        humidity: object.current.humidity,
-        icon: object.current.condition.icon,
+        first_max: max_temp_c_1,
+        second_max: max_temp_c_2,
       });
-      //  res.render("index", { name: object.current.feelslike_f });
-      console.log(object.current.cloud);
-      console.log(object.current.feelslike_f);
-
-      // res.render("index", { title: "" + Object.keys(object) });
     })
     .catch((error) => {
       console.log(error);
     });
 });
-
 // module.exports = app;
-
 module.exports = router;
